8af5acbb4928e779e95343812a05011dd98d2db2
Abel st <abel_st_99@outlook.com>
2019-11-11 20:16:06 -0600

    Main

---
+++ GameText-Abel/Main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<forward_list>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include<array>
+#include<regex>
+#include<random>
+#include<stdexcept>
+
+using namespace std;
